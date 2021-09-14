49c010a87954a5a4d079065d7f652386e5cb1f94
MohammadHasan <rony15987@yahoo.com>
2021-09-14 18:28:42 -0400

    updated controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,18 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  patch "/posted_song/:id" do
+    posted_song = Song.find(params[:id])
+    posted_song.update(posted_song_params)
+    binding.pry
+
+  end
+
+  private 
+
+  def posted_song_params
+    allowed_params = %w(artist_id title duration)
+    params.select {|param,value| allowed_params.include?(param)}
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_09_14_134225) do
 
-  create_table "artist", force: :cascade do |t|
-    t.string "name"
-  end
-
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
