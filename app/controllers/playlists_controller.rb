e151a994a024d46ff23a14d84afa9f08e8e73075
MohammadHasan <rony15987@yahoo.com>
2021-09-16 10:20:58 -0400

    commit

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,6 @@
-class PlaylistsController <ApplicationController 
+class PlaylistsController < ApplicationController 
     get "/playlists" do
-        Artist.all.to_json
-        Song.all.to_json
+        Playlist.all.to_json
+        
       end
 end
